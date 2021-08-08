c1bffef63e07465759e88e3b09b0d07dc6fb747b
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-08-08 10:50:48 +0800

    :zap: add getTotal & get within range

--- models/transactions.go
+++ models/transactions.go
@@ -1,35 +1,44 @@
 package models
 
 type transactions struct {
-        data map[int]int
+	data map[int]int
 }
 
 // TODO: implement this
 // i.e. go test is green
 func NewTransactions(d []int) Transactions {
-        data := make(map[int]int)
+	data := make(map[int]int)
 
-        for i, v := range d {
-                data[i] = v
-        }
+	for i, v := range d {
+		data[i] = v
+	}
 
-        return &transactions{data: data}
+	return &transactions{data: data}
 }
 
 // TODO: implement this
 // i.e. go test is green
 func (t *transactions) Get(idx int) int {
-        return t.data[idx]
+	return t.data[idx]
 }
 
 // TODO: implement this method
 // i.e. go test is green
 func (t *transactions) GetTotal() int {
-        return 0
+	sum := 0
+	for _, v := range t.data {
+		sum += v
+	}
+	return sum
 }
 
 // TODO: implement this method
 // i.e. go test is green
 func (t *transactions) GetTotalWithinRange(i, j int) int {
-        return 0
+	sum := 0
+	for i <= j {
+		sum += t.data[i]
+		i++
+	}
+	return sum
 }
