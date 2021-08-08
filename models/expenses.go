8135428fed943555cf03e2cd366d57287aaaa6af
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-08-08 10:35:30 +0800

    :zap: delete duplicate code

--- models/expenses.go
+++ models/expenses.go
@@ -1,14 +1,6 @@
 package models
 
-type expenses struct {
-	data map[int]int
-}
-
 // TODO: instantiate from transaction model
 func NewExpenses(d []int) Transactions {
 	return NewTransactions(d)
-}
-
-func (e *expenses) Get(idx int) int {
-	return e.data[idx]
 }
--- models/sales.go
+++ models/sales.go
@@ -1,14 +1,6 @@
 package models
 
-type sales struct {
-	data map[int]int
-}
-
 // TODO: instantiate from transaction model
 func NewSales(d []int) Transactions {
 	return NewTransactions(d)
-}
-
-func (s *sales) Get(idx int) int {
-	return s.data[idx]
 }
